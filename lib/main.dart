77fca551a02ffaebe0850442ba8925e7dd675932
Aswaj <aswajcheruvath@gmail.com>
2024-01-01 11:51:02 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,36 @@
+// lib/main.dart
 import 'package:flutter/material.dart';
+import 'mainscreen.dart'; 
 
-void main()
-{
-  runApp(const MaterialApp());
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MainScreen()),
+            );
+          },
+          child: Text('Go to MainScreen'),
+        ),
+      ),
+    );
+  }
 }
---
+++ lib/mainscreen.dart
@@ -0,0 +1,21 @@
+// lib/mainscreen.dart
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text(
+          'Welcome to MainScreen',
+          style: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
