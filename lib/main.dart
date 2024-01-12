da5197bed54f6a0776b29f6b22cd00b2f7280ebe
Midhun NK <Midhunnk2021@gmail.com>
2024-01-12 11:24:36 +0530

    Base

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,5 @@
-// lib/main.dart
 import 'package:flutter/material.dart';
-import 'mainscreen.dart'; 
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => MainScreen()),
-            );
-          },
-          child: const Text('Go to MainScreen'),
-        ),
-      ),
-    );
-  }
+void main(){
+  runApp(const MaterialApp());
 }
