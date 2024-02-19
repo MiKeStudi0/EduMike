949132296496e58fa64790acf12457eaf7ed38f2
Midhun NK <midhunnk2021@gmail.com>
2024-02-20 00:01:40 +0530

    semester ID changed

--- lib/screens/Generator/department.dart
+++ lib/screens/Generator/department.dart
@@ -9,13 +9,12 @@ class FirestoreListView extends StatefulWidget {
 class _FirestoreListViewState extends State<FirestoreListView> {
   String? _selectedUniversityId;
   String? _selectedDegreeId;
-  String? _selectedPropertyId;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Firestore Document IDs Dropdown'),
+        title: Text('Firestore Document IDs Dropdown'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
@@ -30,34 +29,32 @@ class _FirestoreListViewState extends State<FirestoreListView> {
                   AsyncSnapshot<QuerySnapshot> universitySnapshot) {
                 if (universitySnapshot.connectionState ==
                     ConnectionState.waiting) {
-                  return const Center(
+                  return Center(
                     child: CircularProgressIndicator(),
                   );
                 }
                 if (!universitySnapshot.hasData ||
                     universitySnapshot.data!.docs.isEmpty) {
-                  return const Center(
+                  return Center(
                     child: Text('No universities found'),
                   );
                 }
                 return Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Text(
+                    Text(
                       'Select a University ID:',
                       style:
                           TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                     ),
-                    const SizedBox(height: 10),
+                    SizedBox(height: 10),
                     DropdownButton<String>(
                       isExpanded: true,
-                      hint: const Text('Select a University ID'),
+                      hint: Text('Select a University ID'),
                       value: _selectedUniversityId,
                       onChanged: (String? newValue) {
                         setState(() {
                           _selectedUniversityId = newValue;
-                          _selectedDegreeId = null;
-                          _selectedPropertyId = null;
                         });
                       },
                       items: universitySnapshot.data!.docs
@@ -66,12 +63,12 @@ class _FirestoreListViewState extends State<FirestoreListView> {
                           value: document.id,
                           child: Text(
                             document.id,
-                            style: const TextStyle(fontSize: 14),
+                            style: TextStyle(fontSize: 14),
                           ),
                         );
                       }).toList(),
                     ),
-                    const SizedBox(height: 20),
+                    SizedBox(height: 20),
                     if (_selectedUniversityId != null)
                       StreamBuilder(
                         stream: FirebaseFirestore.instance
@@ -82,13 +79,13 @@ class _FirestoreListViewState extends State<FirestoreListView> {
                             AsyncSnapshot<QuerySnapshot> degreeSnapshot) {
                           if (degreeSnapshot.connectionState ==
                               ConnectionState.waiting) {
-                            return const Center(
+                            return Center(
                               child: CircularProgressIndicator(),
                             );
                           }
                           if (!degreeSnapshot.hasData ||
                               degreeSnapshot.data!.docs.isEmpty) {
-                            return const Center(
+                            return Center(
                               child: Text(
                                   'No Degrees found for the selected University'),
                             );
@@ -96,20 +93,19 @@ class _FirestoreListViewState extends State<FirestoreListView> {
                           return Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              const Text(
+                              Text(
                                 'Select a Degree:',
                                 style: TextStyle(
                                     fontSize: 16, fontWeight: FontWeight.bold),
                               ),
-                              const SizedBox(height: 10),
+                              SizedBox(height: 10),
                               DropdownButton<String>(
                                 isExpanded: true,
-                                hint: const Text('Select a Degree'),
+                                hint: Text('Select a Degree'),
                                 value: _selectedDegreeId,
                                 onChanged: (String? newValue) {
                                   setState(() {
                                     _selectedDegreeId = newValue;
-                                    _selectedPropertyId = null;
                                   });
                                 },
                                 items: degreeSnapshot.data!.docs
@@ -118,62 +114,7 @@ class _FirestoreListViewState extends State<FirestoreListView> {
                                     value: document.id,
                                     child: Text(
                                       document.id,
-                                      style: const TextStyle(fontSize: 14),
-                                    ),
-                                  );
-                                }).toList(),
-                              ),
-                            ],
-                          );
-                        },
-                      ),
-                    const SizedBox(height: 20),
-                    if (_selectedDegreeId != null)
-                      StreamBuilder(
-                        stream: FirebaseFirestore.instance
-                            .collection(
-                                '/University/$_selectedUniversityId/Refers/$_selectedDegreeId/Refers')
-                            .snapshots(),
-                        builder: (BuildContext context,
-                            AsyncSnapshot<QuerySnapshot> propertySnapshot) {
-                          if (propertySnapshot.connectionState ==
-                              ConnectionState.waiting) {
-                            return const Center(
-                              child: CircularProgressIndicator(),
-                            );
-                          }
-                          if (!propertySnapshot.hasData ||
-                              propertySnapshot.data!.docs.isEmpty) {
-                            return const Center(
-                              child: Text(
-                                  'No Course found for the selected Degree'),
-                            );
-                          }
-                          return Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Text(
-                                'Select a Course:',
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.bold),
-                              ),
-                              const SizedBox(height: 10),
-                              DropdownButton<String>(
-                                isExpanded: true,
-                                hint: const Text('Select a Course'),
-                                value: _selectedPropertyId,
-                                onChanged: (String? newValue) {
-                                  setState(() {
-                                    _selectedPropertyId = newValue;
-                                  });
-                                },
-                                items: propertySnapshot.data!.docs
-                                    .map((DocumentSnapshot document) {
-                                  return DropdownMenuItem<String>(
-                                    value: document.id,
-                                    child: Text(
-                                      document.id,
-                                      style: const TextStyle(fontSize: 14),
+                                      style: TextStyle(fontSize: 14),
                                     ),
                                   );
                                 }).toList(),
@@ -186,23 +127,21 @@ class _FirestoreListViewState extends State<FirestoreListView> {
                 );
               },
             ),
-            const SizedBox(height: 20),
+            SizedBox(height: 20),
             ElevatedButton(
-              onPressed: (_selectedUniversityId != null &&
-                      _selectedDegreeId != null &&
-                      _selectedPropertyId != null)
-                  ? () {
-                      String path =
-                          '/University/$_selectedUniversityId/Refers/$_selectedDegreeId/Refers/$_selectedPropertyId/';
-                      print('Generated Path: $path');
-                      _createSubcollection(path);
-                    }
-                  : null,
-              child: const Text('Create Subcollection'),
+              onPressed:
+                  (_selectedUniversityId != null && _selectedDegreeId != null)
+                      ? () {
+                          String path =
+                              '/University/$_selectedUniversityId/Refers/$_selectedDegreeId/Refers';
+                          print('Generated Path: $path');
+                          _createSubcollection(path);
+                        }
+                      : null,
+              child: Text('Create Subcollection'),
               style: ElevatedButton.styleFrom(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                textStyle: const TextStyle(fontSize: 16),
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                textStyle: TextStyle(fontSize: 16),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(8),
                 ),
@@ -216,8 +155,49 @@ class _FirestoreListViewState extends State<FirestoreListView> {
   }
 
   Future<void> _createSubcollection(String path) async {
+    List<String> names = [
+      'Aeronautical Engineering',
+      'Agriculture & Farm Engineering',
+      'Applied Electronics and Instrumentation Engineering',
+      'Artificial Intelligence and Data Science',
+      'Artificial Intelligence and Machine Learning',
+      'Automation',
+      'Automobile Engineering',
+      'Biomedical Engineering',
+      'Biotechnology',
+      'Biotechnology and Biochemical Engineering',
+      'Biotechnology and Biochemical Engineering',
+      'Chemical Engineering',
+      'Civil Engineering',
+      'Computer Science and Engineering',
+      'Computer Science and Engineering (Cyber Security)',
+      'Electrical and Electronics Engineering',
+      'Electronics and Biomedical Engineering',
+      'Electronics and Communication Engineering',
+      'Electronics and Computer Engineering',
+      'Electronics and Instrumentation Engineering',
+      'Food Engineering and Technology',
+      'Food Technology',
+      'Industrial Engineering',
+      'Information Technology',
+      'Mechanical Engineering',
+      'Mechanical Engineering (Automobile)',
+      'Mechatronics Engineering',
+      'Metallurgical and Materials Engineering',
+      'Naval Architecture and Ship Building Engineering',
+      'Production Engineering',
+      'Robotics and Automation',
+    ];
+
+    for (String name in names) {
+      await FirebaseFirestore.instance
+          .collection(path)
+          .doc(name)
+          .set({'name': name, 'degree': 'B.Tech'});
+    }
+
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      content: Text('Subcollection created successfully for Path: $path'),
+      content: Text('Subcollection created successfully!'),
     ));
   }
 }
