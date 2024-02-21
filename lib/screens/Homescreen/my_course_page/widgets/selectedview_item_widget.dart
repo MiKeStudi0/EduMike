a235b7ff29c31e2fc4633bb438120da2b84d3bd1
Midhun NK <midhunnk2021@gmail.com>
2024-02-21 18:30:56 +0530

    ERRor FIxed TExt

--- lib/screens/Homescreen/my_course_page/widgets/selectedview_item_widget.dart
+++ lib/screens/Homescreen/my_course_page/widgets/selectedview_item_widget.dart
@@ -13,60 +13,55 @@ class SelectedviewItemWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 16.h, vertical: 19.v),
+      padding: EdgeInsets.only(
+        left: 16.h,
+        top: 19.v,
+        bottom: 19.v,
+      ),
       decoration: AppDecoration.outlineBlueGray.copyWith(
         borderRadius: BorderRadiusStyle.roundedBorder18,
       ),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            width: 194.h,
-            margin: EdgeInsets.only(bottom: 4.v),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(bottom: 1.v),
-                  child: CustomIconButton(
-                    height: 52.adaptSize,
-                    width: 52.adaptSize,
-                    padding: EdgeInsets.all(16.h),
-                    child: CustomImageView(
-                      imagePath: ImageConstant.imgTelevisionBlack900,
-                    ),
-                  ),
-                ),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        courseName, // Use courseName here
-                        style: CustomTextStyles.titleMedium19,
-                      ),
-                      SizedBox(height: 6.v),
-                      Text(
-                        "Description..",
-                        style: theme.textTheme.titleSmall,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+          Padding(
+            padding: EdgeInsets.only(bottom: 1.v),
+            child: CustomIconButton(
+              height: 52.adaptSize,
+              width: 52.adaptSize,
+              padding: EdgeInsets.all(16.h),
+              child: CustomImageView(
+                imagePath: ImageConstant.imgTelevisionBlack900,
+              ),
             ),
           ),
+          SizedBox(width: 10), // Add some spacing between the icon and text
+          Expanded(
+            // Use Expanded to allow the text to occupy remaining space
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    courseName, // Use courseName here
+                    style: CustomTextStyles.titleMedium19,
+                  ),
+                  SizedBox(height: 6.v),
+                  Text(
+                    "Description..",
+                    style: theme.textTheme.titleSmall,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+              width: 10), // Add some spacing between the text and bookmark icon
           CustomImageView(
             imagePath: ImageConstant.imgBookmarkPrimary22x18,
             height: 22.v,
             width: 18.h,
-            margin: EdgeInsets.only(
-              top: 11.v,
-              right: 23.h,
-              bottom: 24.v,
-            ),
           ),
         ],
       ),
