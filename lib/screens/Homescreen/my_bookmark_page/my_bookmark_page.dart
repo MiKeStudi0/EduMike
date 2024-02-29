c7e42047da0c7d358df9ab830c7b42907beb2040
Midhun NK <Midhunnk2021@gmail.com>
2024-02-29 23:05:58 +0530

    BookMark V2

--- lib/screens/Homescreen/my_bookmark_page/my_bookmark_page.dart
+++ lib/screens/Homescreen/my_bookmark_page/my_bookmark_page.dart
@@ -3,20 +3,28 @@ import 'package:edumike/widgets/app_bar/appbar_leading_image.dart';
 import 'package:edumike/widgets/app_bar/appbar_subtitle.dart';
 import 'package:edumike/widgets/app_bar/custom_app_bar_home.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Bookmark {
   final String category;
-  final String title;
-  final String description;
-  final double rating;
-  final int numberOfStudents;
+  final String courseName;
+  final String courseCode;
+  final String courseCredit;
+  final String university;
+  final String degree;
+  final String course;
+  final String semester;
 
   Bookmark({
     required this.category,
-    required this.title,
-    required this.description,
-    required this.rating,
-    required this.numberOfStudents,
+    required this.courseName,
+    required this.courseCode,
+    required this.courseCredit,
+    required this.university,
+    required this.degree,
+    required this.course,
+    required this.semester,
   });
 }
 
@@ -29,31 +37,47 @@ class MyBookmarkPage extends StatefulWidget {
 
 class _MyBookmarkPageState extends State<MyBookmarkPage> {
   List<Bookmark> bookmarks = [];
-  List<String> categories = ['Syllabus', 'Notes'];
-  String selectedCategory = 'Syllabus';
+  List<String> categories = ['SYLLABUS', 'Notes'];
+  String selectedCategory = 'SYLLABUS';
 
   @override
   void initState() {
     super.initState();
-    initializeData();
-    _buildFilterChips(); 
+    loadBookmarkedCourses();
   }
 
-  Future<void> initializeData() async {
-    await Future.delayed(Duration(seconds: 0));
+  Future<void> loadBookmarkedCourses() async {
+    try {
+      User? user = FirebaseAuth.instance.currentUser;
 
-    setState(() {
-      bookmarks = [
-        Bookmark(
-          category: 'Syllabus',
-          title: 'Computer Network...',
-          description: 'Description..',
-          rating: 4.2,
-          numberOfStudents: 7830,
-        ),
-        // Add more bookmarks as needed
-      ];
-    });
+      if (user != null) {
+        // Reference to the user's bookmarks subcollection
+        CollectionReference<Map<String, dynamic>> userBookmarksCollection =
+            FirebaseFirestore.instance.collection('users').doc(user.uid).collection('bookmarks');
+
+        // Get the list of bookmarked courses
+        QuerySnapshot<Map<String, dynamic>> snapshot =
+            await userBookmarksCollection.get();
+
+        setState(() {
+          bookmarks = snapshot.docs.map((doc) {
+            return Bookmark(
+              category: doc['category'] as String,
+              courseName: doc['courseName'] as String,
+              courseCode: doc['courseCode'] as String,
+              courseCredit: doc['courseCredit'] as String,
+              university: doc['university'] as String,
+              degree: doc['degree'] as String,
+              course: doc['course'] as String,
+              semester: doc['semester'] as String,
+            );
+          }).toList();
+        });
+      }
+    } catch (e) {
+      // Handle errors
+      print('Error loading bookmarked courses: $e');
+    }
   }
 
   @override
@@ -120,6 +144,7 @@ class _MyBookmarkPageState extends State<MyBookmarkPage> {
     );
   }
 
+  
   Widget _buildBookmarkItem(Bookmark bookmark) {
     return Container(
       margin: EdgeInsets.only(left: 20.h, right: 20.h, bottom: 20.v),
@@ -186,12 +211,13 @@ class _MyBookmarkPageState extends State<MyBookmarkPage> {
                 ),
                 SizedBox(height: 8.v),
                 Text(
-                  bookmark.title,
+                  bookmark.courseName,
                   style: theme.textTheme.titleMedium,
+                  overflow: TextOverflow.fade,
                 ),
                 SizedBox(height: 5.v),
                 Text(
-                  bookmark.description,
+                  bookmark.courseCode,
                   style: theme.textTheme.titleSmall,
                 ),
                 SizedBox(height: 5.v),
@@ -209,27 +235,30 @@ class _MyBookmarkPageState extends State<MyBookmarkPage> {
                             width: 12.h,
                             margin: EdgeInsets.only(bottom: 2.v),
                           ),
-                          Text(
-                            bookmark.rating.toString(),
-                            style: theme.textTheme.labelMedium,
+                          Padding(
+                            padding: const EdgeInsets.only(left: 4),
+                            child: Text(
+                              "${bookmark.courseCredit}",
+                              style: theme.textTheme.labelMedium,
+                            ),
                           ),
                         ],
                       ),
                     ),
                     Padding(
-                      padding: EdgeInsets.only(left: 16.h),
+                      padding: EdgeInsets.only(left: 5.h),
                       child: Text(
-                        "|",
-                        style: CustomTextStyles.titleSmallBlack900,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 16.h, top: 3.v),
-                      child: Text(
-                        bookmark.numberOfStudents.toString() + " Std",
+                        "Credit",
                         style: theme.textTheme.labelMedium,
                       ),
                     ),
+                    // Padding(
+                    //   padding: EdgeInsets.only(left: 16.h, top: 3.v),
+                    //   child: Text(
+                    //     bookmark.,
+                    //     style: theme.textTheme.labelMedium,
+                    //   ),
+                    // ),
                   ],
                 ),
               ],
@@ -239,6 +268,125 @@ class _MyBookmarkPageState extends State<MyBookmarkPage> {
       ),
     );
   }
+// Widget _buildBookmarkItem(Bookmark bookmark) {
+//   return Container(
+//     margin: EdgeInsets.only(left: 20.h, right: 20.h, bottom: 20.v),
+//     decoration: BoxDecoration(
+//       color: Colors.white,
+//       borderRadius: BorderRadius.circular(20),
+//       boxShadow: [
+//         BoxShadow(
+//           color: Colors.black.withOpacity(0.08),
+//           spreadRadius: 2,
+//           blurRadius: 2,
+//           offset: const Offset(0, 4),
+//         ),
+//       ],
+//     ),
+//     child: Row(
+//       children: [
+//         CustomImageView(
+//           imagePath: ImageConstant.imgImage,
+//           height: 130.adaptSize,
+//           width: 130.adaptSize,
+//           radius: BorderRadius.horizontal(
+//             left: Radius.circular(20.h),
+//           ),
+//         ),
+//         Padding(
+//           padding: EdgeInsets.only(left: 14.h, top: 15.v, bottom: 18.v),
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               SizedBox(
+//                 width: 195.h,
+//                 child: Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                   children: [
+//                     Text(
+//                       bookmark.category,
+//                       style: CustomTextStyles.labelLargeMulishOrangeA700,
+//                     ),
+//                     SizedBox(
+//                       height: 16.v,
+//                       width: 12.h,
+//                       child: Stack(
+//                         alignment: Alignment.center,
+//                         children: [
+//                           CustomImageView(
+//                             imagePath: ImageConstant.imgBookmarkPrimary,
+//                             height: 16.v,
+//                             width: 12.h,
+//                             alignment: Alignment.center,
+//                             onTap: () {},
+//                           ),
+//                           CustomImageView(
+//                             imagePath: ImageConstant.imgBookmarkPrimary,
+//                             height: 16.v,
+//                             width: 12.h,
+//                             alignment: Alignment.center,
+//                           ),
+//                         ],
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//               SizedBox(height: 8.v),
+//               Text(
+//                 bookmark.courseName,
+//                 style: theme.textTheme.titleMedium,
+//               ),
+//               SizedBox(height: 5.v),
+//               Text(
+//                 'Course Code: ${bookmark.courseCode}\nCredit: ${bookmark.courseCredit}\n',
+//                 style: theme.textTheme.titleSmall,
+//               ),
+//               SizedBox(height: 5.v),
+//               Row(
+//                 children: [
+//                   Container(
+//                     width: 32.h,
+//                     margin: EdgeInsets.only(top: 3.v),
+//                     child: Row(
+//                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                       children: [
+//                         CustomImageView(
+//                           imagePath: ImageConstant.imgSignal,
+//                           height: 11.v,
+//                           width: 12.h,
+//                           margin: EdgeInsets.only(bottom: 2.v),
+//                         ),
+//                         Text(
+//                           bookmark.courseCredit,
+//                           style: theme.textTheme.labelMedium,
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                   Padding(
+//                     padding: EdgeInsets.only(left: 16.h),
+//                     child: Text(
+//                       "|",
+//                       style: CustomTextStyles.titleSmallBlack900,
+//                     ),
+//                   ),
+//                   Padding(
+//                     padding: EdgeInsets.only(left: 16.h, top: 3.v),
+//                     child: Text(
+//                       bookmark.courseCode,
+//                       style: theme.textTheme.labelMedium,
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ],
+//           ),
+//         ),
+//       ],
+//     ),
+//   );
+// }
 
   PreferredSizeWidget _buildAppBar(BuildContext context) {
     return CustomAppBar(
