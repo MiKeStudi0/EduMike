b1491bcd73be36229dac9e11411ecafb73a8c83d
Midhun NK <Midhunnk2021@gmail.com>
2024-03-02 11:07:40 +0530

    indox V4

--- lib/screens/Homescreen/indoxmainpage_page/indoxmainpage_page.dart
+++ lib/screens/Homescreen/indoxmainpage_page/indoxmainpage_page.dart
@@ -29,7 +29,7 @@ class _IndoxmainpagePageState extends State<IndoxmainpagePage> {
                   }
 
                   if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(child: CircularProgressIndicator());
+                    return const Center(child: CircularProgressIndicator());
                   }
 
                   List<Message> messages = snapshot.data!.docs
@@ -45,7 +45,7 @@ class _IndoxmainpagePageState extends State<IndoxmainpagePage> {
                         future: getNickname(message.senderId),
                         builder: (context, nicknameSnapshot) {
                           if (nicknameSnapshot.connectionState == ConnectionState.waiting) {
-                            return SizedBox.shrink();
+                            return const SizedBox.shrink();
                           }
 
                           String senderNickname = nicknameSnapshot.data ?? 'Unknown User';
@@ -66,7 +66,7 @@ class _IndoxmainpagePageState extends State<IndoxmainpagePage> {
 
   PreferredSizeWidget _buildAppBar(BuildContext context) {
     return AppBar(
-      title: Text('Inbox'),
+      title: const Text('Inbox'),
      
     );
   }
@@ -75,8 +75,8 @@ class _IndoxmainpagePageState extends State<IndoxmainpagePage> {
     return Align(
       alignment: message.senderId == getUserId() ? Alignment.centerRight : Alignment.centerLeft,
       child: Container(
-        margin: EdgeInsets.all(8.0),
-        padding: EdgeInsets.all(12.0),
+        margin: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(12.0),
         decoration: BoxDecoration(
           color: message.senderId == getUserId() ? Colors.blue : Colors.grey,
           borderRadius: BorderRadius.circular(10.0),
@@ -89,24 +89,24 @@ class _IndoxmainpagePageState extends State<IndoxmainpagePage> {
           children: [
             Text(
               '$senderNickname:',
-              style: TextStyle(
+              style: const TextStyle(
                 color: Colors.white,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 4.0),
+            const SizedBox(height: 4.0),
             Text(
               message.text,
-              style: TextStyle(color: Colors.white),
+              style: const TextStyle(color: Colors.white),
             ),
-            SizedBox(height: 4.0),
+            const SizedBox(height: 4.0),
             Align(
               alignment: Alignment.centerRight,
               child: Text(
                 message.timestamp != null
                     ? _formatDateTime(message.timestamp!)
                     : '',
-                style: TextStyle(color: Colors.white70),
+                style: const TextStyle(color: Colors.white70),
               ),
             ),
           ],
@@ -117,7 +117,7 @@ class _IndoxmainpagePageState extends State<IndoxmainpagePage> {
 
   Widget _buildInputField() {
     return Container(
-      padding: EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(8.0),
       decoration: BoxDecoration(
         color: Colors.grey[200],
         boxShadow: [
@@ -125,7 +125,7 @@ class _IndoxmainpagePageState extends State<IndoxmainpagePage> {
             color: Colors.grey.withOpacity(0.5),
             spreadRadius: 1,
             blurRadius: 3,
-            offset: Offset(0, 2),
+            offset: const Offset(0, 2),
           ),
         ],
       ),
@@ -133,7 +133,7 @@ class _IndoxmainpagePageState extends State<IndoxmainpagePage> {
         children: [
           Expanded(
             child: Container(
-              constraints: BoxConstraints(
+              constraints: const BoxConstraints(
                 maxHeight: 100.0,
               ),
               child: TextField(
@@ -148,14 +148,14 @@ class _IndoxmainpagePageState extends State<IndoxmainpagePage> {
                   ),
                   filled: true,
                   fillColor: Colors.white,
-                  contentPadding: EdgeInsets.all(12.0),
+                  contentPadding: const EdgeInsets.all(12.0),
                 ),
               ),
             ),
           ),
-          SizedBox(width: 8.0),
+          const SizedBox(width: 8.0),
           IconButton(
-            icon: Icon(Icons.send),
+            icon: const Icon(Icons.send),
             onPressed: () {
               sendMessage(_textController.text);
               _textController.clear();
