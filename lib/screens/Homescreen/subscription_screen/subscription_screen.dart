0adfe0ed61fa61cea0d89ba4ca781afa3915bd6e
Midhun NK <midhunnk2021@gmail.com>
2024-03-11 17:14:39 +0530

    cleaned

--- lib/screens/Homescreen/subscription_screen/subscription_screen.dart
+++ lib/screens/Homescreen/subscription_screen/subscription_screen.dart
@@ -1,121 +1,121 @@
-import '../subscription_screen/widgets/viewhierarchy_item_widget.dart';
-import 'package:edumike/core/app_export.dart';
-import 'package:edumike/screens/Homescreen/homemainpage_page/homemainpage_page.dart';
-import 'package:edumike/screens/Homescreen/indoxmainpage_page/indoxmainpage_page.dart';
-import 'package:edumike/screens/Homescreen/my_bookmark_page/my_bookmark_page.dart';
-import 'package:edumike/screens/Homescreen/my_course_page/my_course_page.dart';
-import 'package:edumike/screens/Homescreen/profiles_page/profiles_page.dart';
-import 'package:edumike/widgets/app_bar/appbar_leading_image_home.dart';
-import 'package:edumike/widgets/app_bar/appbar_subtitle.dart';
-import 'package:edumike/widgets/app_bar/custom_app_bar_home.dart';
-import 'package:edumike/widgets/custom_bottom_bar.dart';
-import 'package:flutter/material.dart';
+// import '../subscription_screen/widgets/viewhierarchy_item_widget.dart';
+// import 'package:edumike/core/app_export.dart';
+// import 'package:edumike/screens/Homescreen/homemainpage_page/homemainpage_page.dart';
+// import 'package:edumike/screens/Homescreen/indoxmainpage_page/indoxmainpage_page.dart';
+// import 'package:edumike/screens/Homescreen/my_bookmark_page/my_bookmark_page.dart';
+// import 'package:edumike/screens/Homescreen/my_course_page/my_course_page.dart';
+// import 'package:edumike/screens/Homescreen/profiles_page/profiles_page.dart';
+// import 'package:edumike/widgets/app_bar/appbar_leading_image_home.dart';
+// import 'package:edumike/widgets/app_bar/appbar_subtitle.dart';
+// import 'package:edumike/widgets/app_bar/custom_app_bar_home.dart';
+// import 'package:edumike/widgets/custom_bottom_bar.dart';
+// import 'package:flutter/material.dart';
 
-// ignore_for_file: must_be_immutable
-class SubscriptionScreen extends StatelessWidget {
-  SubscriptionScreen({Key? key}) : super(key: key);
+// // ignore_for_file: must_be_immutable
+// class SubscriptionScreen extends StatelessWidget {
+//   SubscriptionScreen({Key? key}) : super(key: key);
 
-  GlobalKey<NavigatorState> navigatorKey = GlobalKey();
+//   GlobalKey<NavigatorState> navigatorKey = GlobalKey();
 
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-            appBar: _buildAppBar(context),
-            body: Container(
-                width: double.maxFinite,
-                padding: EdgeInsets.symmetric(horizontal: 34.h, vertical: 25.v),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("University",
-                          style: CustomTextStyles.titleMediumBold),
-                      SizedBox(height: 17.v),
-                      _buildViewHierarchy(context)
-                    ])),
-            bottomNavigationBar: _buildBottomBar(context)));
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//         child: Scaffold(
+//             appBar: _buildAppBar(context),
+//             body: Container(
+//                 width: double.maxFinite,
+//                 padding: EdgeInsets.symmetric(horizontal: 34.h, vertical: 25.v),
+//                 child: Column(
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     children: [
+//                       Text("University",
+//                           style: CustomTextStyles.titleMediumBold),
+//                       SizedBox(height: 17.v),
+//                       _buildViewHierarchy(context)
+//                     ])),
+//             bottomNavigationBar: _buildBottomBar(context)));
+//   }
 
-  /// Section Widget
-  PreferredSizeWidget _buildAppBar(BuildContext context) {
-    return CustomAppBar(
-        leadingWidth: 61.h,
-        leading: AppbarLeadingImage(
-            imagePath: ImageConstant.imgArrowDown,
-            margin: EdgeInsets.only(left: 35.h, top: 17.v, bottom: 18.v),
-            onTap: () {
-              onTapArrowDown(context);
-            }),
-        title: AppbarSubtitle(
-            text: "Subscription", margin: EdgeInsets.only(left: 12.h)));
-  }
+//   /// Section Widget
+//   PreferredSizeWidget _buildAppBar(BuildContext context) {
+//     return CustomAppBar(
+//         leadingWidth: 61.h,
+//         leading: AppbarLeadingImage(
+//             imagePath: ImageConstant.imgArrowDown,
+//             margin: EdgeInsets.only(left: 35.h, top: 17.v, bottom: 18.v),
+//             onTap: () {
+//               onTapArrowDown(context);
+//             }),
+//         title: AppbarSubtitle(
+//             text: "Subscription", margin: EdgeInsets.only(left: 12.h)));
+//   }
 
-  /// Section Widget
-  Widget _buildViewHierarchy(BuildContext context) {
-    return ListView.separated(
-        physics: NeverScrollableScrollPhysics(),
-        shrinkWrap: true,
-        separatorBuilder: (context, index) {
-          return SizedBox(height: 12.v);
-        },
-        itemCount: 3,
-        itemBuilder: (context, index) {
-          return ViewhierarchyItemWidget(onTapTxtSubscribeText: () {
-            onTapTxtSubscribeText(context);
-          });
-        });
-  }
+//   /// Section Widget
+//   Widget _buildViewHierarchy(BuildContext context) {
+//     return ListView.separated(
+//         physics: NeverScrollableScrollPhysics(),
+//         shrinkWrap: true,
+//         separatorBuilder: (context, index) {
+//           return SizedBox(height: 12.v);
+//         },
+//         itemCount: 3,
+//         itemBuilder: (context, index) {
+//           return ViewhierarchyItemWidget(onTapTxtSubscribeText: () {
+//             onTapTxtSubscribeText(context);
+//           });
+//         });
+//   }
 
-  /// Section Widget
-  Widget _buildBottomBar(BuildContext context) {
-    return CustomBottomBar(onChanged: (BottomBarEnum type) {
-      Navigator.pushNamed(navigatorKey.currentContext!, getCurrentRoute(type));
-    });
-  }
+//   /// Section Widget
+//   Widget _buildBottomBar(BuildContext context) {
+//     return CustomBottomBar(onChanged: (BottomBarEnum type) {
+//       Navigator.pushNamed(navigatorKey.currentContext!, getCurrentRoute(type));
+//     });
+//   }
 
-  ///Handling route based on bottom click actions
-  String getCurrentRoute(BottomBarEnum type) {
-    switch (type) {
-      case BottomBarEnum.Home:
-        return AppRoutes.homemainpagePage;
-      case BottomBarEnum.Mycourses:
-        return AppRoutes.myCoursePage;
-      case BottomBarEnum.Indox:
-        return AppRoutes.indoxmainpagePage;
-      case BottomBarEnum.Bookmark:
-        return AppRoutes.myBookmarkPage;
-      case BottomBarEnum.Profile:
-        return AppRoutes.profilesPage;
-      default:
-        return "/";
-    }
-  }
+//   ///Handling route based on bottom click actions
+//   String getCurrentRoute(BottomBarEnum type) {
+//     switch (type) {
+//       case BottomBarEnum.Home:
+//         return AppRoutes.homemainpagePage;
+//       case BottomBarEnum.Mycourses:
+//         return AppRoutes.myCoursePage;
+//       case BottomBarEnum.Indox:
+//         return AppRoutes.indoxmainpagePage;
+//       case BottomBarEnum.Bookmark:
+//         return AppRoutes.myBookmarkPage;
+//       case BottomBarEnum.Profile:
+//         return AppRoutes.profilesPage;
+//       default:
+//         return "/";
+//     }
+//   }
 
-  ///Handling page based on route
-  Widget getCurrentPage(String currentRoute) {
-    switch (currentRoute) {
-      case AppRoutes.homemainpagePage:
-        return HomemainpagePage();
-      case AppRoutes.myCoursePage:
-        return MyCoursePage();
-      case AppRoutes.indoxmainpagePage:
-        return IndoxmainpagePage();
-      case AppRoutes.myBookmarkPage:
-        return MyBookmarkPage();
-      case AppRoutes.profilesPage:
-        return ProfilesPage();
-      default:
-        return DefaultWidget();
-    }
-  }
+//   ///Handling page based on route
+//   Widget getCurrentPage(String currentRoute) {
+//     switch (currentRoute) {
+//       case AppRoutes.homemainpagePage:
+//         return HomemainpagePage();
+//       case AppRoutes.myCoursePage:
+//         return MyCoursePage();
+//       case AppRoutes.indoxmainpagePage:
+//         return IndoxmainpagePage();
+//       case AppRoutes.myBookmarkPage:
+//         return MyBookmarkPage();
+//       case AppRoutes.profilesPage:
+//         return ProfilesPage();
+//       default:
+//         return DefaultWidget();
+//     }
+//   }
 
-  /// Navigates to the addSubscribeScreen when the action is triggered.
-  onTapTxtSubscribeText(BuildContext context) {
-    Navigator.pushNamed(context, AppRoutes.addSubscribeScreen);
-  }
+//   /// Navigates to the addSubscribeScreen when the action is triggered.
+//   onTapTxtSubscribeText(BuildContext context) {
+//     Navigator.pushNamed(context, AppRoutes.addSubscribeScreen);
+//   }
 
-  /// Navigates to the homemainpageContainerScreen when the action is triggered.
-  onTapArrowDown(BuildContext context) {
-    Navigator.pushNamed(context, AppRoutes.homemainpageContainerScreen);
-  }
-}
+//   /// Navigates to the homemainpageContainerScreen when the action is triggered.
+//   onTapArrowDown(BuildContext context) {
+//     Navigator.pushNamed(context, AppRoutes.homemainpageContainerScreen);
+//   }
+// }
--- lib/widgets/custom_bottom_bar.dart
+++ lib/widgets/custom_bottom_bar.dart
@@ -1,240 +1,239 @@
-import 'package:edumike/core/app_export.dart';
-import 'package:edumike/screens/Dropdownlist.dart';
-import 'package:edumike/screens/Homescreen/my_course_page/my_course_page.dart';
-import 'package:edumike/screens/Homescreen/profiles_page/profiles_page.dart';
-import 'package:edumike/screens/uploadscreen.dart';
-import 'package:flutter/material.dart';
+// import 'package:edumike/core/app_export.dart';
+// import 'package:edumike/screens/Dropdownlist.dart';
+// import 'package:edumike/screens/Homescreen/my_course_page/my_course_page.dart';
+// import 'package:edumike/screens/Homescreen/profiles_page/profiles_page.dart';
+// import 'package:flutter/material.dart';
 
-import '../screens/Homescreen/homemainpage_page/homemainpage_page.dart';
+// import '../screens/Homescreen/homemainpage_page/homemainpage_page.dart';
 
-// ignore: must_be_immutable
-class CustomBottomBar extends StatefulWidget {
-  CustomBottomBar({this.onChanged});
+// // ignore: must_be_immutable
+// class CustomBottomBar extends StatefulWidget {
+//   CustomBottomBar({this.onChanged});
 
-  Function(BottomBarEnum)? onChanged;
+//   Function(BottomBarEnum)? onChanged;
 
-  @override
-  CustomBottomBarState createState() => CustomBottomBarState();
-}
+//   @override
+//   CustomBottomBarState createState() => CustomBottomBarState();
+// }
 
-class CustomBottomBarState extends State<CustomBottomBar> {
-  int selectedIndex = 0;
+// class CustomBottomBarState extends State<CustomBottomBar> {
+//   int selectedIndex = 0;
 
-  List<BottomMenuModel> bottomMenuList = [
-    BottomMenuModel(
-      icon: ImageConstant.imgNavHomeNot,
-      activeIcon: ImageConstant.imgNavHome,
-      title: "Home".toUpperCase(),
-      type: BottomBarEnum.Home,
-    ),
-    BottomMenuModel(
-      icon: ImageConstant.imgNavMyCourses,
-      activeIcon: ImageConstant.imgNavMyCoursesActive,
-      title: "My Courses".toUpperCase(),
-      type: BottomBarEnum.Mycourses,
-    ),
-    BottomMenuModel(
-      icon: ImageConstant.imgNavIndox,
-      activeIcon: ImageConstant.imgNavIndoxActive,
-      title: "Indox".toUpperCase(),
-      type: BottomBarEnum.Indox,
-    ),
-    BottomMenuModel(
-      icon: ImageConstant.imgNavBookMark,
-      activeIcon: ImageConstant.imgNavBookMarkActive,
-      title: "Book Mark".toUpperCase(),
-      type: BottomBarEnum.Bookmark,
-    ),
-    BottomMenuModel(
-      icon: ImageConstant.imgNavProfile,
-      activeIcon: ImageConstant.imgNavProfileActive,
-      title: "Profile".toUpperCase(),
-      type: BottomBarEnum.Profile,
-    )
-  ];
+//   List<BottomMenuModel> bottomMenuList = [
+//     BottomMenuModel(
+//       icon: ImageConstant.imgNavHomeNot,
+//       activeIcon: ImageConstant.imgNavHome,
+//       title: "Home".toUpperCase(),
+//       type: BottomBarEnum.Home,
+//     ),
+//     BottomMenuModel(
+//       icon: ImageConstant.imgNavMyCourses,
+//       activeIcon: ImageConstant.imgNavMyCoursesActive,
+//       title: "My Courses".toUpperCase(),
+//       type: BottomBarEnum.Mycourses,
+//     ),
+//     BottomMenuModel(
+//       icon: ImageConstant.imgNavIndox,
+//       activeIcon: ImageConstant.imgNavIndoxActive,
+//       title: "Indox".toUpperCase(),
+//       type: BottomBarEnum.Indox,
+//     ),
+//     BottomMenuModel(
+//       icon: ImageConstant.imgNavBookMark,
+//       activeIcon: ImageConstant.imgNavBookMarkActive,
+//       title: "Book Mark".toUpperCase(),
+//       type: BottomBarEnum.Bookmark,
+//     ),
+//     BottomMenuModel(
+//       icon: ImageConstant.imgNavProfile,
+//       activeIcon: ImageConstant.imgNavProfileActive,
+//       title: "Profile".toUpperCase(),
+//       type: BottomBarEnum.Profile,
+//     )
+//   ];
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 100.v,
-      decoration: BoxDecoration(
-        color: appTheme.gray5001,
-      ),
-      child: BottomNavigationBar(
-        backgroundColor: Colors.transparent,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        selectedFontSize: 0,
-        elevation: 0,
-        currentIndex: selectedIndex,
-        type: BottomNavigationBarType.fixed,
-        items: List.generate(bottomMenuList.length, (index) {
-          return BottomNavigationBarItem(
-            icon: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CustomImageView(
-                  imagePath: bottomMenuList[index].icon,
-                  height: 20.v,
-                  width: 18.h,
-                  color: appTheme.blueGray90001,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 4.v),
-                  child: Text(
-                    bottomMenuList[index].title ?? "",
-                    style: theme.textTheme.labelSmall!.copyWith(
-                      color: appTheme.blueGray90001,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            activeIcon: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CustomImageView(
-                  imagePath: bottomMenuList[index].activeIcon,
-                  height: 19.v,
-                  width: 18.h,
-                  color: theme.colorScheme.primary,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 5.v),
-                  child: Text(
-                    bottomMenuList[index].title ?? "",
-                    style: CustomTextStyles.labelSmallPrimary.copyWith(
-                      color: theme.colorScheme.primary,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            label: '',
-          );
-        }),
-        onTap: (index) {
-          selectedIndex = index;
-          widget.onChanged?.call(bottomMenuList[index].type);
-          setState(() {});
-        },
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height: 100.v,
+//       decoration: BoxDecoration(
+//         color: appTheme.gray5001,
+//       ),
+//       child: BottomNavigationBar(
+//         backgroundColor: Colors.transparent,
+//         showSelectedLabels: false,
+//         showUnselectedLabels: false,
+//         selectedFontSize: 0,
+//         elevation: 0,
+//         currentIndex: selectedIndex,
+//         type: BottomNavigationBarType.fixed,
+//         items: List.generate(bottomMenuList.length, (index) {
+//           return BottomNavigationBarItem(
+//             icon: Column(
+//               mainAxisSize: MainAxisSize.min,
+//               crossAxisAlignment: CrossAxisAlignment.center,
+//               children: [
+//                 CustomImageView(
+//                   imagePath: bottomMenuList[index].icon,
+//                   height: 20.v,
+//                   width: 18.h,
+//                   color: appTheme.blueGray90001,
+//                 ),
+//                 Padding(
+//                   padding: EdgeInsets.only(top: 4.v),
+//                   child: Text(
+//                     bottomMenuList[index].title ?? "",
+//                     style: theme.textTheme.labelSmall!.copyWith(
+//                       color: appTheme.blueGray90001,
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//             activeIcon: Column(
+//               mainAxisSize: MainAxisSize.min,
+//               crossAxisAlignment: CrossAxisAlignment.center,
+//               children: [
+//                 CustomImageView(
+//                   imagePath: bottomMenuList[index].activeIcon,
+//                   height: 19.v,
+//                   width: 18.h,
+//                   color: theme.colorScheme.primary,
+//                 ),
+//                 Padding(
+//                   padding: EdgeInsets.only(top: 5.v),
+//                   child: Text(
+//                     bottomMenuList[index].title ?? "",
+//                     style: CustomTextStyles.labelSmallPrimary.copyWith(
+//                       color: theme.colorScheme.primary,
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//             label: '',
+//           );
+//         }),
+//         onTap: (index) {
+//           selectedIndex = index;
+//           widget.onChanged?.call(bottomMenuList[index].type);
+//           setState(() {});
+//         },
+//       ),
+//     );
+//   }
+// }
 
-enum BottomBarEnum {
-  Home,
-  Mycourses,
-  Indox,
-  Bookmark,
-  Profile,
-}
+// enum BottomBarEnum {
+//   Home,
+//   Mycourses,
+//   Indox,
+//   Bookmark,
+//   Profile,
+// }
 
-class BottomMenuModel {
-  BottomMenuModel({
-    required this.icon,
-    required this.activeIcon,
-    this.title,
-    required this.type,
-  });
+// class BottomMenuModel {
+//   BottomMenuModel({
+//     required this.icon,
+//     required this.activeIcon,
+//     this.title,
+//     required this.type,
+//   });
 
-  String icon;
+//   String icon;
 
-  String activeIcon;
+//   String activeIcon;
 
-  String? title;
+//   String? title;
 
-  BottomBarEnum type;
-}
+//   BottomBarEnum type;
+// }
 
-class DefaultWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      padding: EdgeInsets.all(10),
-      child: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Please replace the respective Widget here',
-              style: TextStyle(
-                fontSize: 18,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+// class DefaultWidget extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       color: Colors.white,
+//       padding: EdgeInsets.all(10),
+//       child: Center(
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Text(
+//               'Please replace the respective Widget here',
+//               style: TextStyle(
+//                 fontSize: 18,
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-class HomemainpageContainerScreen extends StatelessWidget {
-  HomemainpageContainerScreen({Key? key}) : super(key: key);
+// class HomemainpageContainerScreen extends StatelessWidget {
+//   HomemainpageContainerScreen({Key? key}) : super(key: key);
 
-  GlobalKey<NavigatorState> navigatorKey = GlobalKey();
+//   GlobalKey<NavigatorState> navigatorKey = GlobalKey();
 
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: appTheme.whiteA700,
-        body: SingleChildScrollView(
-          physics: NeverScrollableScrollPhysics(),
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            child: Navigator(
-              key: navigatorKey,
-              initialRoute: AppRoutes.homemainpagePage,
-              onGenerateRoute: (routeSetting) => PageRouteBuilder(
-                pageBuilder: (ctx, ani, ani1) =>
-                    getCurrentPage(routeSetting.name!),
-                transitionDuration: Duration(seconds: 0),
-              ),
-            ),
-          ),
-        ),
-        bottomNavigationBar: CustomBottomBar(),
-      ),
-    );
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: Scaffold(
+//         backgroundColor: appTheme.whiteA700,
+//         body: SingleChildScrollView(
+//           physics: NeverScrollableScrollPhysics(),
+//           child: Container(
+//             height: MediaQuery.of(context).size.height,
+//             child: Navigator(
+//               key: navigatorKey,
+//               initialRoute: AppRoutes.homemainpagePage,
+//               onGenerateRoute: (routeSetting) => PageRouteBuilder(
+//                 pageBuilder: (ctx, ani, ani1) =>
+//                     getCurrentPage(routeSetting.name!),
+//                 transitionDuration: Duration(seconds: 0),
+//               ),
+//             ),
+//           ),
+//         ),
+//         bottomNavigationBar: CustomBottomBar(),
+//       ),
+//     );
+//   }
 
-  String getCurrentRoute(BottomBarEnum type) {
-    switch (type) {
-      case BottomBarEnum.Home:
-        return AppRoutes.homemainpagePage;
-      case BottomBarEnum.Mycourses:
-        return AppRoutes.myCoursePage;
-      case BottomBarEnum.Indox:
-        return AppRoutes.uploadscreen;
-      case BottomBarEnum.Bookmark:
-        return AppRoutes.listview;
-      case BottomBarEnum.Profile:
-        return AppRoutes.profilesPage;
-      default:
-        return "/";
-    }
-  }
+//   String getCurrentRoute(BottomBarEnum type) {
+//     switch (type) {
+//       case BottomBarEnum.Home:
+//         return AppRoutes.homemainpagePage;
+//       case BottomBarEnum.Mycourses:
+//         return AppRoutes.myCoursePage;
+//       case BottomBarEnum.Indox:
+//         return AppRoutes.uploadscreen;
+//       case BottomBarEnum.Bookmark:
+//         return AppRoutes.listview;
+//       case BottomBarEnum.Profile:
+//         return AppRoutes.profilesPage;
+//       default:
+//         return "/";
+//     }
+//   }
 
-  Widget getCurrentPage(String currentRoute) {
-    switch (currentRoute) {
-      case AppRoutes.homemainpagePage:
-        return HomemainpagePage();
-      case AppRoutes.myCoursePage:
-        return MyCoursePage();
-      case AppRoutes.uploadscreen:
-        return UploadDataPage();
-      case AppRoutes.listview:
-        return FirestoreListView();
-      case AppRoutes.profilesPage:
-        return ProfilesPage();
-      default:
-        return DefaultWidget();
-    }
-  }
-}
+//   Widget getCurrentPage(String currentRoute) {
+//     switch (currentRoute) {
+//       case AppRoutes.homemainpagePage:
+//         return HomemainpagePage();
+//       case AppRoutes.myCoursePage:
+//         return MyCoursePage();
+//       case AppRoutes.uploadscreen:
+//         return UploadDataPage();
+//       case AppRoutes.listview:
+//         return FirestoreListView();
+//       case AppRoutes.profilesPage:
+//         return ProfilesPage();
+//       default:
+//         return DefaultWidget();
+//     }
+//   }
+// }
