48ede50ead73564aeb35fa338598d053c344034a
Midhun NK <midhunnk2021@gmail.com>
2024-03-24 16:24:27 +0530

    Remove Bookmark V6

--- lib/screens/Homescreen/my_bookmark_page/my_bookmark_page.dart
+++ lib/screens/Homescreen/my_bookmark_page/my_bookmark_page.dart
@@ -3,6 +3,9 @@ import 'package:edumike/screens/Homescreen/remove_bookmark_screen/remove_bookmar
 import 'package:edumike/widgets/app_bar/appbar_leading_image.dart';
 import 'package:edumike/widgets/app_bar/appbar_subtitle.dart';
 import 'package:edumike/widgets/app_bar/custom_app_bar_home.dart';
+import 'package:edumike/widgets/custom_elevated_button.dart';
+import 'package:edumike/widgets/custom_outlined_button_home.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -170,12 +173,246 @@ class _MyBookmarkPageState extends State<MyBookmarkPage> {
           child: Container(
             // Set a specific height, you can adjust this value based on your needs
             height: MediaQuery.of(context).size.height * 0.7,
-            child: RemoveBookmarkScreen( category: Category, courseName: CourseName, courseCode: CourseCode, courseCredit: CourseCredit, university: University, degree: Degree, course: Course, semester: Semester),
+            child: Removebookmark(context,Category,CourseName,CourseCode,CourseCredit,University,Degree,Course,Semester),
           ),
         ),
       );
     },
   );
+}
+
+Widget Removebookmark(BuildContext context, String? category, String? courseName, String? courseCode, String? courseCredit, String? university, String? degree, String? course, String? semester) {
+  return Container(
+              //width: 100.h,
+              padding: EdgeInsets.symmetric(
+                horizontal: 34.h,
+                vertical: 31.v,
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 43.h),
+                    child: Text(
+                      "Remove From Bookmark?",
+                      style: theme.textTheme.titleLarge,
+                    ),
+                  ),
+                  SizedBox(height: 23.v),
+                  _buildMainStack(context,category,courseName,courseCode,courseCredit,university,degree,course,semester),
+                  SizedBox(height: 29.v),
+                  _buildCancelButtons(context,category,courseName,courseCode,courseCredit,),
+                  SizedBox(height: 5.v),
+                ],
+              ),
+            );
+}
+
+ /// Section Widget
+  Widget _buildMainStack(BuildContext context, String? category, String? courseName, String? courseCode, String? courseCredit, String? university, String? degree, String? course, String? semester) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.08),
+            spreadRadius: 2,
+            blurRadius: 2,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          CustomImageView(
+            imagePath: ImageConstant.imgImage,
+            height: 130.adaptSize,
+            width: 130.adaptSize,
+            radius: BorderRadius.horizontal(
+              left: Radius.circular(20.h),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(left: 10.h, top: 15.v, bottom: 18.v),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: 195.h,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          category!,
+                          style: CustomTextStyles.labelLargeMulishOrangeA700,
+                        ),
+                        SizedBox(
+                          height: 16.v,
+                          width: 12.h,
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              CustomImageView(
+                                imagePath: ImageConstant.imgBookmarkPrimary,
+                                height: 16.v,
+                                width: 12.h,
+                                alignment: Alignment.center,
+                                onTap: () {
+                                },
+                              ),
+                              // CustomImageView(
+                              //   imagePath: ImageConstant.imgBookmarkPrimary,
+                              //   height: 16.v,
+                              //   width: 12.h,
+                              //   alignment: Alignment.center,
+                              // ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 8.v),
+                  Text(
+                    courseName!,
+                    style: theme.textTheme.titleMedium,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(height: 5.v),
+                  Text(
+                    courseCode!,
+                    style: theme.textTheme.titleSmall,
+                  ),
+                  SizedBox(height: 5.v),
+                  Row(
+                    children: [
+                      Container(
+                        width: 32.h,
+                        margin: EdgeInsets.only(top: 3.v),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            CustomImageView(
+                              imagePath: ImageConstant.imgSignal,
+                              height: 11.v,
+                              width: 12.h,
+                              margin: EdgeInsets.only(bottom: 2.v),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 4),
+                              child: Text(
+                                courseCredit!,
+                                style: theme.textTheme.labelMedium,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 5.h, top: 3.v),
+                        child: Text(
+                          "Credit",
+                          style: theme.textTheme.labelMedium,
+                        ),
+                      ),
+                      // Padding(
+                      //   padding: EdgeInsets.only(left: 16.h, top: 3.v),
+                      //   child: Text(
+                      //     bookmark.,
+                      //     style: theme.textTheme.labelMedium,
+                      //   ),
+                      // ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  /// Section Widget
+/// Section Widget
+Widget _buildCancelButtons(BuildContext context, String? category, String? courseName, String? courseCode, String? courseCredit,) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      CustomOutlinedButton(
+        width: 140.h,
+        text: "Cancel",
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+      CustomElevatedButton(
+        width: 206.h,
+        text: "Yes, Remove",
+        onPressed: () {
+          _removeBookmark(context, category, courseName, courseCode, courseCredit,);
+        },
+      ),
+    ],
+  );
+}
+void _removeBookmark(BuildContext context, String? category, String? courseName, String? courseCode, String? courseCredit) async {
+  try {
+    User? user = FirebaseAuth.instance.currentUser;
+
+    if (user != null) {
+      // Reference to the user's bookmarks subcollection
+      CollectionReference<Map<String, dynamic>> userBookmarksCollection =
+          FirebaseFirestore.instance
+              .collection('users')
+              .doc(user.uid)
+              .collection('bookmarks');
+
+      // Query for the bookmarked course
+      QuerySnapshot<Map<String, dynamic>> snapshot =
+          await userBookmarksCollection
+              .where('category', isEqualTo: category)
+              .where('courseName', isEqualTo: courseName)
+              .where('courseCode', isEqualTo: courseCode)
+              .limit(1)
+              .get();
+
+      // Check if the bookmark exists
+      if (snapshot.docs.isNotEmpty) {
+        // Get the document ID of the bookmarked course
+        String bookmarkId = snapshot.docs.first.id;
+
+        // Delete the bookmarked course document
+        await userBookmarksCollection.doc(bookmarkId).delete();
+
+        // Pop the screen
+        Navigator.pop(context);
+
+        // Show a success message or perform any other action as needed
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text("Bookmark removed successfully"),
+        ));
+
+        // Refresh UI
+        setState(() {
+          bookmarks.removeWhere((bookmark) =>
+              bookmark.category == category &&
+              bookmark.courseName == courseName &&
+              bookmark.courseCode == courseCode &&
+              bookmark.courseCredit == courseCredit);
+        });
+      }
+    }
+  } catch (e) {
+    // Handle errors
+    print('Error removing bookmark: $e');
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text("Failed to remove bookmark"),
+    ));
+  }
 }
 
 
@@ -306,125 +543,7 @@ class _MyBookmarkPageState extends State<MyBookmarkPage> {
       ),
     );
   }
-// Widget _buildBookmarkItem(Bookmark bookmark) {
-//   return Container(
-//     margin: EdgeInsets.only(left: 20.h, right: 20.h, bottom: 20.v),
-//     decoration: BoxDecoration(
-//       color: Colors.white,
-//       borderRadius: BorderRadius.circular(20),
-//       boxShadow: [
-//         BoxShadow(
-//           color: Colors.black.withOpacity(0.08),
-//           spreadRadius: 2,
-//           blurRadius: 2,
-//           offset: const Offset(0, 4),
-//         ),
-//       ],
-//     ),
-//     child: Row(
-//       children: [
-//         CustomImageView(
-//           imagePath: ImageConstant.imgImage,
-//           height: 130.adaptSize,
-//           width: 130.adaptSize,
-//           radius: BorderRadius.horizontal(
-//             left: Radius.circular(20.h),
-//           ),
-//         ),
-//         Padding(
-//           padding: EdgeInsets.only(left: 14.h, top: 15.v, bottom: 18.v),
-//           child: Column(
-//             crossAxisAlignment: CrossAxisAlignment.start,
-//             children: [
-//               SizedBox(
-//                 width: 195.h,
-//                 child: Row(
-//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                   children: [
-//                     Text(
-//                       bookmark.category,
-//                       style: CustomTextStyles.labelLargeMulishOrangeA700,
-//                     ),
-//                     SizedBox(
-//                       height: 16.v,
-//                       width: 12.h,
-//                       child: Stack(
-//                         alignment: Alignment.center,
-//                         children: [
-//                           CustomImageView(
-//                             imagePath: ImageConstant.imgBookmarkPrimary,
-//                             height: 16.v,
-//                             width: 12.h,
-//                             alignment: Alignment.center,
-//                             onTap: () {},
-//                           ),
-//                           CustomImageView(
-//                             imagePath: ImageConstant.imgBookmarkPrimary,
-//                             height: 16.v,
-//                             width: 12.h,
-//                             alignment: Alignment.center,
-//                           ),
-//                         ],
-//                       ),
-//                     ),
-//                   ],
-//                 ),
-//               ),
-//               SizedBox(height: 8.v),
-//               Text(
-//                 bookmark.courseName,
-//                 style: theme.textTheme.titleMedium,
-//               ),
-//               SizedBox(height: 5.v),
-//               Text(
-//                 'Course Code: ${bookmark.courseCode}\nCredit: ${bookmark.courseCredit}\n',
-//                 style: theme.textTheme.titleSmall,
-//               ),
-//               SizedBox(height: 5.v),
-//               Row(
-//                 children: [
-//                   Container(
-//                     width: 32.h,
-//                     margin: EdgeInsets.only(top: 3.v),
-//                     child: Row(
-//                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                       children: [
-//                         CustomImageView(
-//                           imagePath: ImageConstant.imgSignal,
-//                           height: 11.v,
-//                           width: 12.h,
-//                           margin: EdgeInsets.only(bottom: 2.v),
-//                         ),
-//                         Text(
-//                           bookmark.courseCredit,
-//                           style: theme.textTheme.labelMedium,
-//                         ),
-//                       ],
-//                     ),
-//                   ),
-//                   Padding(
-//                     padding: EdgeInsets.only(left: 16.h),
-//                     child: Text(
-//                       "|",
-//                       style: CustomTextStyles.titleSmallBlack900,
-//                     ),
-//                   ),
-//                   Padding(
-//                     padding: EdgeInsets.only(left: 16.h, top: 3.v),
-//                     child: Text(
-//                       bookmark.courseCode,
-//                       style: theme.textTheme.labelMedium,
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//             ],
-//           ),
-//         ),
-//       ],
-//     ),
-//   );
-// }
+
 
   PreferredSizeWidget _buildAppBar(BuildContext context) {
     return CustomAppBar(
